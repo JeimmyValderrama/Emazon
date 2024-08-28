a34e70aec0af7568ff620db1accdf74b98fbace6
Jeimmy <valderrama.jeimmy@gmail.com>
2024-08-27 21:04:42 -0500

    primer

---
+++ src/test/java/com/emzon/stock/EmazonStock/EmazonStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emzon.stock.EmazonStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
